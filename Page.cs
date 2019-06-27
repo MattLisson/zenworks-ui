a63d42e50828bdf4a8c46b263f93468645d6040b
McFood <matt@litech.com.au>
2019-06-27 21:53:31 +0800

    Add a constructor with a Lazy ViewModel to Page.
    + This is much easier than trying to swap a page out inthe Tabbed Page.
    + Init is quick enough for just the views.

--- Page.cs
+++ Page.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 
 namespace Zenworks.UI {
     public class BasePage : ContentPage {
@@ -8,8 +9,10 @@ namespace Zenworks.UI {
     }
 
     public class BasePage<TViewModel> : BasePage where TViewModel : ViewModel {
-        private TViewModel viewModel;
-        public TViewModel ViewModel {
+        private readonly Lazy<TViewModel>? maybeLazyViewModel;
+
+        private TViewModel? viewModel;
+        public TViewModel? ViewModel {
             get => viewModel;
             set {
                 if (viewModel != null) {
@@ -23,11 +26,17 @@ namespace Zenworks.UI {
             }
         }
 
+        public TViewModel ForceViewModel => ViewModel ?? maybeLazyViewModel!.Value;
+
         public bool WarnOnBackButtonWithUnsavedChanges { get; set; } = false;
 
 #pragma warning disable CS8618 // Non-nullable field is uninitialized.
         // TODO: Fix the null viewmodel for Design-Time view.
         public BasePage() { }
+
+        public BasePage(Lazy<TViewModel> model) {
+            maybeLazyViewModel = model;
+        }
 
         public BasePage(TViewModel model) {
 #pragma warning restore CS8618 // Non-nullable field is uninitialized.
@@ -36,17 +45,20 @@ namespace Zenworks.UI {
 
         protected override void OnAppearing() {
             base.OnAppearing();
-            viewModel.OnAppearing();
+            if (maybeLazyViewModel != null && ViewModel == null) {
+                ViewModel = maybeLazyViewModel.Value;
+            }
+            ViewModel?.OnAppearing();
         }
         protected override bool OnBackButtonPressed() {
-            if (!WarnOnBackButtonWithUnsavedChanges || !ViewModel.HasChanged) {
+            if (ViewModel == null || !WarnOnBackButtonWithUnsavedChanges || !ViewModel.HasChanged) {
                 return base.OnBackButtonPressed();
             }
             // Begin an asyncronous task on the UI thread because we intend to ask the users permission.
             Device.BeginInvokeOnMainThread(async () => {
                 if (await DisplayAlert("Exit page?", "You have unsaved changes, are you sure you want to exit?", "Discard Changes", "Stay here")) {
                     base.OnBackButtonPressed();
-                    ViewModel.FinishTask();
+                    ViewModel?.FinishTask();
                 }
             });
 
