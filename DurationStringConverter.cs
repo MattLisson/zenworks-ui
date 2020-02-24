dd6db5d83e197e91cbb73f438acdbb5dd0f297af
McFood <matt@litech.com.au>
2020-02-24 21:48:10 +0800

    Add DurationToWholeSeconds Converter for formatting.

--- DurationStringConverter.cs
+++ DurationStringConverter.cs
@@ -29,4 +29,26 @@ namespace Zenworks.UI {
             return result.Value;
         }
     }
+
+    public class DurationToWholeSeconds : IValueConverter {
+        private static readonly DurationPattern hm = DurationPattern.CreateWithCurrentCulture("S's'");
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value is Duration) {
+                return hm.Format((Duration)value);
+            } else {
+                return value.ToString();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (!(value is string)) {
+                return value;
+            }
+            ParseResult<Duration> result = hm.Parse(value as string);
+            if (!result.Success) {
+                return value;
+            }
+            return result.Value;
+        }
+    }
 }
